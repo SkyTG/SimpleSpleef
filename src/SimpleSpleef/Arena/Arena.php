3c9a2eb1e9b6c658b3efccb0a34cc6347e90d328
SuperChipsLP <nikilp26@gmail.com>
2014-10-20 11:46:56 +0200

    Setting spawns for arenas

--- src/SimpleSpleef/Arena/Arena.php
+++ src/SimpleSpleef/Arena/Arena.php
@@ -2,6 +2,7 @@
 
 namespace SimpleSpleef\Arena;
 
+use pocketmine\level\Position;
 use pocketmine\Player;
 
 class Arena {
@@ -11,6 +12,9 @@ class Arena {
 
     //Array of players that are in the arena
     private $players = array();
+
+    //Arena spawn
+    private $spawn;
 
     /*
      * Create a new arena
@@ -22,12 +26,17 @@ class Arena {
 
     /*
      * Add a player to the arena
+     * Returns: void
      */
     public function addPlayer(Player $player)
     {
         $this->players[$player->getName()] = $player;
     }
 
+    /*
+     * Remove a player from the arena
+     * Returns: bool
+     */
     public function removePlayer(Player $player)
     {
         if(isset($this->players[$player->getName()]))
@@ -47,6 +56,24 @@ class Arena {
     public function getName()
     {
         return $this->arena_name;
+    }
+
+    public function getSpawn()
+    {
+        return $this->spawn;
+    }
+
+    /*
+     * Setters
+     */
+    public function setName($name)
+    {
+        $this->arena_name = $name;
+    }
+
+    public function setSpawn(Position $pos)
+    {
+        $this->spawn = $pos;
     }
 
 } 
