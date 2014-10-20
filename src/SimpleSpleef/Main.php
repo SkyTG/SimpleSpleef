cffed2c27e75a76656d47b50083b2b576209f0b4
SuperChipsLP <nikilp26@gmail.com>
2014-10-20 12:11:58 +0200

    Save and load arenas

--- src/SimpleSpleef/Main.php
+++ src/SimpleSpleef/Main.php
@@ -2,6 +2,7 @@
 
 namespace SimpleSpleef;
 
+use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase;
 use SimpleSpleef\Arena\Arena;
 
@@ -14,10 +15,9 @@ class Main extends PluginBase {
 
     }
 
-
     /*
      * Create an arena
-     * Returns: bool
+     * Returns: Arena
      */
     public function createArena($name)
     {
@@ -25,7 +25,7 @@ class Main extends PluginBase {
         {
             $arena = new Arena($name);
             $this->arenas[$arena->getName()] = $arena;
-            return true;
+            return $this->arenas[$arena->getName()];
         }
         else
         {
@@ -35,7 +35,7 @@ class Main extends PluginBase {
 
     /*
      * Get an arena by it's name
-     * Returns: Success: Arena - Failure: false
+     * Returns: Arena
      */
     public function getArenaByName($name)
     {
@@ -47,6 +47,48 @@ class Main extends PluginBase {
         {
             return false;
         }
+    }
+
+    /*
+     * Save an arenas data as json in the resources
+     * Returns: void
+     */
+    public function saveArena(Arena $arena)
+    {
+        $name = $arena->getName();
+        $spawn = $arena->getSpawn();
+        //Make spawn out of string
+        if($spawn instanceof Position)
+        {
+            $spawn = $spawn->getX()." ".$spawn->getY()." ".$spawn->getZ()." ".$spawn->getLevel()->getName();
+        }
+
+        $arena_data = array(
+            "name" => $name,
+            "spawn" => $spawn
+        );
+
+        $arena_data = json_encode($arena_data);
+        file_put_contents($this->getDataFolder()."/arenas/".$name."/data.json", $arena_data);
+    }
+
+    /*
+     * Load a saved arena
+     * Returns: Arena
+     */
+    public function loadArena($name)
+    {
+        $data = file_get_contents($this->getDataFolder()."/arenas/".$name."/data.json");
+        $data = json_decode($data, true);
+
+        $arena = $this->createArena($data["name"]);
+        if($arena instanceof Arena)
+        {
+            $spawn = explode(" ", $data["spawn"]);
+            $spawn = new Position($spawn[0], $spawn[1], $spawn[2], $this->getServer()->getLevelByName($spawn[3]));
+            $arena->setSpawn($spawn);
+        }
+        return $arena;
     }
 
 } 
