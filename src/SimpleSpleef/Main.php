47b7a192be92c4342583b64bf65af8658d140818
SuperChipsLP <nikilp26@gmail.com>
2014-10-20 11:50:44 +0200

    Create an arena

--- src/SimpleSpleef/Main.php
+++ src/SimpleSpleef/Main.php
@@ -3,12 +3,34 @@
 namespace SimpleSpleef;
 
 use pocketmine\plugin\PluginBase;
+use SimpleSpleef\Arena\Arena;
 
 class Main extends PluginBase {
+
+    private $arenas = array();
 
     public function onEnable()
     {
 
+    }
+
+
+    /*
+     * Create an arena
+     * Returns: bool
+     */
+    public function createArena($name)
+    {
+        if(!isset($this->arenas[$name]))
+        {
+            $arena = new Arena($name);
+            $this->arenas[$arena->getName()] = $arena;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 } 
